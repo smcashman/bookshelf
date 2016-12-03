867746338a84d30b6cdc0d2bf74a125cd752aad3
Sara Cashman <saramcashman@gmail.com>
2016-12-02 21:14:51 -0500

    front end put

--- public/app.js
+++ public/app.js
@@ -1,34 +1,29 @@
 var booklist = {
-    "bookInfo": [
-        {
-            "id": "1",
-            "title": "Harry Potter and the Sorcerer's Stone",
-            "author": "J.K. Rowling",
-            "source": "borrowed from a friend",
-            "collection": "to be read"
-        },
-        {
-            "id": "2",
-            "title": "War and Peace",
-            "author": "Tolstoy",
-            "source": "purchased at Goodwill",
-            "collection": "classics"
-        },
-        {
-            "id": "3",
-            "title": "Madame Secretary",
-            "author": "Madeleine Albright",
-            "source": "birthday present",
-            "collection": "loaned out"
-        },
-        {
-            "id": "4",
-            "title": "Computing: A Concise History",
-            "author": "Paul Ceruzzi",
-            "source": "library",
-            "collection": "to be read"
-        }
-    ]
+    "bookInfo": [{
+        "id": "1",
+        "title": "Harry Potter and the Sorcerer's Stone",
+        "author": "J.K. Rowling",
+        "source": "borrowed from a friend",
+        "collection": "to be read"
+    }, {
+        "id": "2",
+        "title": "War and Peace",
+        "author": "Tolstoy",
+        "source": "purchased at Goodwill",
+        "collection": "classics"
+    }, {
+        "id": "3",
+        "title": "Madame Secretary",
+        "author": "Madeleine Albright",
+        "source": "birthday present",
+        "collection": "loaned out"
+    }, {
+        "id": "4",
+        "title": "Computing: A Concise History",
+        "author": "Paul Ceruzzi",
+        "source": "library",
+        "collection": "to be read"
+    }]
 };
 
 // function getBooklist(callbackFn) {
@@ -50,52 +45,87 @@ var booklist = {
 //     getBooklist(displayBooklist);
 // }
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-// $('#submitButton').click(function(){
-//      $.ajax({
-//             url: "http://localhost:8080/books",
-//             data: "{title:'anything'}",
-//             type: "post",
-//             success: function(){
-//               $('#submitSuccess').html('<p>Book successfully added</p>');
-                
-//             }
-//         })
-// })
+    // $('#submitButton').click(function(){
+    //      $.ajax({
+    //             url: "http://localhost:8080/books",
+    //             data: "{title:'anything'}",
+    //             type: "post",
+    //             success: function(){
+    //               $('#submitSuccess').html('<p>Book successfully added</p>');
 
-$("#showBooks").click(function(){
-   
-  $.getJSON("http://localhost:8080/books", function(data){
-        // var myResponse = (data.title);
-         $.each(data, function(index, value){
-        
-      $(".displayExistingTitles").append('<div><p><span class="bookTitle">'+value.title+' </span> '+value.author+' '+value.source+' <button class="deleteButton" id='+value._id+'>Delete</button><button class="editButton" id='+value._id+'>Edit</button></p></div>')
+    //             }
+    //         })
+    // })
+
+    $("#showBooks").click(function() {
+
+        $.getJSON("http://localhost:8080/books", function(data) {
+            // var myResponse = (data.title);
+            $.each(data, function(index, value) {
+
+                $(".displayExistingTitles").append('<div><p class='+value._id+'><span class="bookTitle">' + value.title + ' </span><span class="bookAuthor"> ' + value.author + ' </span><span class="bookSource"> ' + value.source + ' </span><button class="deleteButton">Delete</button><button class="editButton" id=' + value._id + '>Edit</button></p></div>')
+            });
+            $('.deleteButton').click(function() {
+                var buttonId = $(this).attr('id');
+                $.ajax({
+                    url: "http://localhost:8080/books/" + buttonId,
+                    type: "Delete",
+                    success: function() {
+                        $('button#' + buttonId).closest('div').remove();
+                        console.log("item deleted");
+
+                    }
+                })
+
+            })
+
+            $('.editButton').click(function() {
+
+                $(this).text('Update').addClass('editSubmitButton');
+
+                var title = $(this).parent("p").children("span.bookTitle").text();
+                $(this).parent("p").children('span.bookTitle').html("<input id='editTitle' name='editTitle' type='text' value=" + title + ">");
+
+                var author = $(this).parent("p").children("span.bookAuthor").text();
+                $(this).parent("p").children('span.bookAuthor').html("<input id='editAuthor' name='editAuthor' type='text' value=" + author + ">");
+
+                var source = $(this).parent("p").children("span.bookSource").text();
+                $(this).parent("p").children('span.bookSource').html("<input id='editSource' name='editSource' type='text' value=" + source + ">");
+
+                console.log(title);
+                console.log(author);
+                console.log(source);
+
+                $('.editSubmitButton').click(function(){
+
+                    var buttonClassUpdate = $(this).parent('p').attr('class')
+                   var editedTitle = $('p.'+buttonClassUpdate).children('span').children('input#editTitle').val();
+                   console.log(editedTitle);
+                    $.ajax({
+                    url: "http://localhost:8080/books/" + buttonClassUpdate,
+                    type: "Put",
+                    success: function() {
+                         
+                          console.log(buttonClassUpdate);
+
+                        
+                        
+                        // $('button#' + buttonIdUpdate).closest('div').remove();
+                        
+
+                    }
+                })
+                })
+
+            })
+
+
+        });
+
+
     });
-        $('.deleteButton').click(function(){
-            var buttonId = $(this).attr('id');
-        $.ajax({
-            url: "http://localhost:8080/books/"+buttonId,
-            type: "delete",
-            success: function(){
-              $('button#'+buttonId).closest('div').remove();
-                console.log("item deleted");
-                
-            }
-        })
-        
-        })
-
-        $('.editButton').click(function(){
-            var title = $(this).parent("p").children("span.bookTitle").text();
-            $(this).parent("p").children('span.bookTitle').html("<input type='text' value="+title+">")
-            console.log(title);
-
-        })
-    });
-
-
-});
 
 
 
